c185b5995be442bbb2b28d1421cca7f12c80dd6e
Guinsly Mondesir <guinslym@gmail.com>
2020-05-02 21:16:38 -0400

    add twilio

--- sp_ask_presence_alert/__init__.py
+++ sp_ask_presence_alert/__init__.py
@@ -8,6 +8,12 @@ import time
 from twisted.internet import task, reactor
 import sys
 
+from twilio.rest import Client
+from environs import Env
+env = Env()
+# Read .env into os.environ
+env.read_env()
+
 #from initialization import get_log_formatter
 
 queues = ['scholars-portal', "scholars-portal-txt", "clavardez"]
@@ -65,13 +71,7 @@ def check_service_and_insert_to_db():
             # app_log.error("Can't add value in database"+ str(e) )
             pass
 
-
-if __name__ == '__main__':
-    timeout = 60.0
-    # try:
-    #     Service.delete().execute() 
-    # except:
-    #     pass
+def get_presence():
     create_table()
     check_service_and_insert_to_db()
     query = Service.select()
@@ -79,4 +79,42 @@ if __name__ == '__main__':
     print(len(queues))
     for service in query:
         print(service)
+
+def send_sms(web, clavardez, sms):
+    account_sid = env("ACCOUNT_SID")
+    auth_token = env("AUTH_TOKEN")
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        body="web-en:\t{0}\nweb-fr:\t{1}\nSMS:\t{2}\n".format(web, clavardez, sms),
+        from_=env("FROM"),
+        to=env("TO")
+    )
+
+def verify_Ask_service(min_alert_minute):
+    fr_result = Service.select().where((Service.status=="unavailable") and (Service.queue=="clavardez"))
+    sms_result = Service.select().where((Service.status=="unavailable") and (Service.queue=="scholars-portal-txt"))
+    for res in result:
+        print(res)
+    if len(result) >=min_alert_minute:
+        clavardez = len(Service.select().where((Service.status=="unavailable") and (Service.queue=="clavardez")))
+        sms = len(Service.select().where((Service.status=="unavailable") and (Service.queue=="scholars-portal-txt")))
+        web = len(Service.select().where((Service.status=="unavailable") and (Service.queue=="scholars-portal")))
+        print("web-en:\t{0}\web-fr:\t{1}\nSMS:\t{2}\n".format(web, clavardez, sms))
+        #send_sms(web, clavardez, sms)
+        sys.exit()
+
+if __name__ == '__main__':
+    min_alert_minute = 5
+    Service.delete().execute() 
+    counter = 0
+    while counter < min_alert_minute:
+        get_presence()
+        time.sleep(60)
+        counter +=1
+    
+    # After 10 min .. check this
+    verify_Ask_service(min_alert_minute)
+
+    breakpoint()
+
 
